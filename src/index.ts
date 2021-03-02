fcbb5361036051abc32e8f96c2a7c4688b00d4d1
Tom Meyer <tmeyer@yext.com>
2021-03-02 16:02:33 -0500

    Restructure index.js to provide StatefulCore instance.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
-import { provideCore, Matcher } from '@yext/answers-core';
-import StatefulCore from './core';
-import StateListener from './state-listener';
+import { provideCore, AnswersConfig } from '@yext/answers-core';
+import StatefulCore from './stateful-core';
 import StateManager from './state-manager';
 
-async function test() {
-  const answersCore = provideCore({
-    apiKey: 'df4b24f4075800e5e9705090c54c6c13', 
-    experienceKey: 'rosetest', 
-    locale: 'en' 
-  });
+export function provideStatefulCore(config: AnswersConfig): StatefulCore {
+  const answersCore = provideCore(config);
   const stateManager = new StateManager();
-  const statefulCore = new StatefulCore(answersCore, stateManager);
 
-  const queryListener: StateListener<string> = {
-    valueAccessor: state => state.query.query,
-    callback: query => console.log(query)
-  };
-  statefulCore.addListener(queryListener);
-
-  statefulCore.setQuery('Yext');
-  statefulCore.setVerticalKey('people');
-
-  statefulCore.setQuery('Consulting');
-
-  await statefulCore.executeVerticalQuery();
-
-  statefulCore.setQuery('Yext');
-  await statefulCore.executeUniversalQuery();
+  return new StatefulCore(answersCore, stateManager);
 }
 
-test();
+export { StatefulCore };

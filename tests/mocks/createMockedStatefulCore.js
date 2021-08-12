752e7229ed996fd50c5dfbfb126b335f8df5a092
Connor Anderson <canderson@yext.com>
2021-08-12 12:59:07 -0400

    update naming

--- tests/mocks/createMockedStatefulCore.js
+++ tests/mocks/createMockedStatefulCore.js
@@ -4,13 +4,13 @@ import StatefulCore from '../../src/stateful-core';
 /**
  * Creates a stateful core instance with a mocked answers core
  * 
- * @param {Partial<AnswersCore>} mockedCore Answers core overrides
+ * @param {Partial<AnswersCore>} mockedAnswersCore Answers core overrides
  * @param {Partial<State>} initialState The initial state of the stateful core
  * @returns 
  */
-export function createMockedStatefulCore(mockedCore = {}, initialState = {}) {
+export function createMockedStatefulCore(mockedAnswersCore = {}, initialState = {}) {
   const reduxStateManager = new ReduxStateManager();
-  const statefulCore = new StatefulCore(mockedCore, reduxStateManager);
+  const statefulCore = new StatefulCore(mockedAnswersCore, reduxStateManager);
   statefulCore.setState(initialState);
   return statefulCore;
 }

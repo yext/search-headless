539e706ffca5a9c5af4f56fe75d48c4c6d42af26
Oliver Shi <oshi@yext.com>
2021-09-13 11:12:39 -0400

    allow empty searches (#23)
    J=SLAP-1578
    TEST=auto

---
+++ tests/integration/universalsearch.ts
@@ -0,0 +1,11 @@
+import { UniversalSearchRequest } from '@yext/answers-core';
+import { createMockedStatefulCore } from '../mocks/createMockedStatefulCore';
+
+it('vertical searches send blank queries by default', async () => {
+  const mockSearch = jest.fn((_request: UniversalSearchRequest) => Promise.resolve({}));
+  const statefulCore = createMockedStatefulCore({
+    universalSearch: mockSearch
+  });
+  await statefulCore.executeUniversalQuery();
+  expect(mockSearch.mock.calls[0][0].query).toEqual('');
+});

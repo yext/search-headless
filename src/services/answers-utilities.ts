c226116747f4a758883d5f9c69bd0cef43159eed
Connor Anderson <canderson@yext.com>
2021-10-22 11:22:42 -0400

    add inheritDoc

--- src/services/answers-utilities.ts
+++ src/services/answers-utilities.ts
@@ -1,5 +1,6 @@
 import { DisplayableFacet } from '@yext/answers-core';
 
+/** {@inheritDoc AnswersUtilitiesImpl} */
 export default interface AnswersUtilities {
   searchThroughFacet: (facet: DisplayableFacet, searchTerm: string) => DisplayableFacet
 }

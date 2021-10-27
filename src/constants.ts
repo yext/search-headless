08f76cd2aa3e9438b6df8975065a316c42acfffe
Oliver Shi <oshi@yext.com>
2021-10-27 15:05:23 -0400

    remove redux init constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,3 @@
  * The headlessId automatically given to the first AnswersHeadless instance created.
  */
 export const DEFAULT_ID = 'main';
-
-/**
- * A redux implementation detail. This is called when a store is first initialized, to set
- * the initial state.
- */
-export const REDUX_INIT_ACTION = '@@INIT';

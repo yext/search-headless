849f8c9a785de3b00e23da470bc3af6665e3a275
Connor Anderson <canderson@yext.com>
2021-11-08 11:55:14 -0500

    space

--- tests/integration/directanswer.ts
+++ tests/integration/directanswer.ts
@@ -1,5 +1,5 @@
 import { createMockedAnswersHeadless } from '../mocks/createMockedAnswersHeadless';
-import { FeaturedSnippetDirectAnswer, DirectAnswerType, Source} from '@yext/answers-core';
+import { FeaturedSnippetDirectAnswer, DirectAnswerType, Source } from '@yext/answers-core';
 
 const initialState = {
   query: {

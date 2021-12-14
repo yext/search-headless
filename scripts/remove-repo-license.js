d294565da498c0e9cb5d25e1462c5efee6bd857a
Yen Truong <ytruong@yext.com>
2021-12-14 09:52:37 -0500

    Update remove repo license script
        - update script to consider three cases when removing current repo from third party notices file:
          1) current repo have its own license section => remove the entire section
          2) current repo share license section with another package => remove repo name and udpate grammar
          3) current repo share license section with 2(+) other packages => remove repo name
    
        J=SLAP-1754
        TEST=manual
    
        run scripts based on the three scenarios above and see that it works accordingly

--- scripts/remove-repo-license.js
+++ scripts/remove-repo-license.js
@@ -9,40 +9,74 @@ function removeRepoLicense() {
   const noticesFile = './THIRD-PARTY-NOTICES';
 
   const noticesFileContents = fs.readFileSync(noticesFile, 'utf8');
-  const noticesWithoutRepoLicense = getNoticesWithoutReactSiteStarterLicense(noticesFileContents);
+  const noticesWithoutRepoLicense = getNoticesWithoutRepoLicense(noticesFileContents);
 
   fs.writeFileSync(noticesFile, noticesWithoutRepoLicense);
 }
 
 /**
- * Removes the repo's library license from the 3rd party notices file
- * contents and returns the updated file as a string
+ * Return number of packages that share the same license as the current repo 
+ * in the third party notices file
+ * @param {string} fileContents The contents of the 3rd party notices file
+ * @param {number} licenseStart the index at the start of the repo's license section
+ */
+function countNumPackagesWithSharedLicense(fileContents, licenseStart) {
+  const packageNameRegex = / - .*@.*/;
+  let atPackageName = true;
+  let currentFileIndex = licenseStart + packageNameRegex.exec(fileContents.slice(licenseStart)).index;
+  let numPackagesWithSharedLicense = 0;
+  while (atPackageName) {
+    const line = fileContents.slice(currentFileIndex, fileContents.indexOf('\n', currentFileIndex));
+    atPackageName = packageNameRegex.test(line);
+    if (atPackageName) {
+      numPackagesWithSharedLicense++;
+    }
+    currentFileIndex += line.length + 1;
+  }
+  return numPackagesWithSharedLicense;
+}
+
+/**
+ * Removes the repo's package name and/or library license from the 3rd party
+ * notices file contents and returns the updated file as a string
  *
  * @param {string} fileContents The contents of the 3rd party notices file
  * @returns {string}
  */
-function getNoticesWithoutReactSiteStarterLicense(fileContents) {
-  const packageName = packageJson.name;
-  const repoLine = ` - ${packageName}`;
-  const licenseStart = 'The following NPM package may be included in this product:';
-  const divider = '-----------';
-  const numNewLinesAfterDivider = 2;
+function getNoticesWithoutRepoLicense(fileContents) {
+  const repoLine = ` - ${packageJson.name}`;
+  const divider = '-----------\n';
 
   const indexOfRepoLine = fileContents.indexOf(repoLine);
   if (indexOfRepoLine === -1) {
     return fileContents;
   }
 
-  const startOfRepoLicense = fileContents.lastIndexOf(licenseStart, indexOfRepoLine);
-  const endOfRepoLicense =
-    fileContents.indexOf(divider, startOfRepoLicense)
-    + divider.length
-    + numNewLinesAfterDivider;
+  // determine how many packages share the same license as current repo
+  const startIndexOfRepoDivider = fileContents.lastIndexOf(divider, indexOfRepoLine);
+  const startIndexOfRepoLicense = startIndexOfRepoDivider === -1 ? 0 : startIndexOfRepoDivider + divider.length;
+  const numPackagesWithSharedLicense = countNumPackagesWithSharedLicense(fileContents, startIndexOfRepoLicense);
 
-  const fileBeforeRepoLicense = fileContents.slice(0, startOfRepoLicense);
-  const fileAfterRepoLicense = fileContents.slice(endOfRepoLicense);
+  if (numPackagesWithSharedLicense === 1) {
+    // remove repo's license
+    const endIndexOfRepoLicense = fileContents.indexOf(divider, startIndexOfRepoLicense) + divider.length;
+    return fileContents.slice(0, startIndexOfRepoLicense) + fileContents.slice(endIndexOfRepoLicense);
+  } else {
+    // remove repo's package name
+    const endIndexOfRepoLine = fileContents.indexOf('\n', indexOfRepoLine) + 1;
+    let modifiedFileContents = fileContents.slice(0, indexOfRepoLine) + fileContents.slice(endIndexOfRepoLine);
 
-  return fileBeforeRepoLicense + fileAfterRepoLicense;
+    if (numPackagesWithSharedLicense === 2) {
+      modifiedFileContents = modifiedFileContents.replace(
+        'The following NPM packages may be included in this product:', 
+        'The following NPM package may be included in this product:');
+      modifiedFileContents = modifiedFileContents.replace(
+        'These packages each contain the following license and notice below:', 
+        'This package contains the following license and notice below:'
+      );
+    }
+    return modifiedFileContents;
+  }
 }
 
 removeRepoLicense();
